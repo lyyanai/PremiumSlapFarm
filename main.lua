a74f57fd60cc6ee8e2ed7b6b2351b61e68780d1c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-24 10:45:56 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Checking...",Text = "Checking User HWID..." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "ok"})
 local HWIDList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/HWID_WhiteList/main/main.lua'))()
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
 for i,v in pairs(HWIDList) do
   if v == HWID then
     game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Whitelisted!",Text = "Successfully Whitelisted! Hold on, script is loading." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+  
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+
   else
     game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "You're not whitelisted!" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
   end
