93c91734b7834bd151a6f92119b39208db86c10f
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-24 09:43:51 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+local HWIDList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/HWID_WhiteList/main/main.lua'))()
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+for i,v in pairs(HWIDList) do
+  if v == HWID then
+    game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Whitelisted!",Text = "Successfully Whitelisted! Hold on, script is loading." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+  else
+    game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "You're not whitelisted" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
+  end
+end
